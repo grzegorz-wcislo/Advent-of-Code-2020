3a264ac27dac1128c416cedbed3f46f9c687d799
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-12-03 21:43:02 +0100

    Refactor and add tests

--- lib/aoc/day01.ex
+++ lib/aoc/day01.ex
@@ -16,59 +16,38 @@ defmodule Aoc.Day01 do
     |> Enum.to_list()
   end
 
+  def k_combinations(elements, k) when k <= 0 or length(elements) < k do
+    []
+  end
+
+  def k_combinations(elements, k) when k == 1 do
+    Enum.map(elements, &[&1])
+  end
+
+  def k_combinations(elements, k) do
+    elements_with_index = Stream.with_index(elements)
+
+    elements_with_index
+    |> Stream.flat_map(fn {element1, index1} ->
+      available_elements =
+        elements_with_index
+        |> Enum.filter(fn {_, index2} -> index2 > index1 end)
+        |> Enum.map(&elem(&1, 0))
+
+      Enum.map(k_combinations(available_elements, k - 1), &[element1 | &1])
+    end)
+    |> Enum.to_list()
+  end
+
   def sum2_to(sum, elements) do
-    elements_with_index = Enum.with_index(elements)
-
-    element_pairs =
-      Enum.flat_map(elements_with_index, fn {element, index} ->
-        Enum.map(
-          Enum.filter(elements_with_index, fn {_, i} ->
-            i > index
-          end),
-          fn {element2, _index2} ->
-            {element, element2}
-          end
-        )
-      end)
-
-    case Enum.find(element_pairs, fn {e1, e2} -> e1 + e2 == sum end) do
+    case Enum.find(k_combinations(elements, 2), fn [e1, e2] -> e1 + e2 == sum end) do
       nil -> :error
       result -> {:ok, result}
     end
   end
 
   def sum3_to(sum, elements) do
-    elements_with_index = Enum.with_index(elements)
-
-    element_triplets =
-      Enum.flat_map(
-        elements_with_index,
-        fn {element1, index1} ->
-          Enum.flat_map(
-            Enum.filter(
-              elements_with_index,
-              fn {_, index2} ->
-                index2 > index1
-              end
-            ),
-            fn {element2, index2} ->
-              Enum.map(
-                Enum.filter(
-                  elements_with_index,
-                  fn {_, index3} ->
-                    index3 > index2
-                  end
-                ),
-                fn {element3, _index3} ->
-                  {element1, element2, element3}
-                end
-              )
-            end
-          )
-        end
-      )
-
-    case Enum.find(element_triplets, fn {e1, e2, e3} -> e1 + e2 + e3 == sum end) do
+    case Enum.find(k_combinations(elements, 3), fn [e1, e2, e3] -> e1 + e2 + e3 == sum end) do
       nil -> :error
       result -> {:ok, result}
     end
--- test/aoc/day01_test.exs
+++ test/aoc/day01_test.exs
@@ -1,15 +1,56 @@
 defmodule Aoc.Day01Test do
   use ExUnit.Case
 
-  test "1 and 3 sum2 to 4" do
-    assert {:ok, {1, 3}} == Aoc.Day01.sum2_to(4, [1, 2, 3, 4])
+  describe "&k_combinations/2" do
+    test "[] has 0 combinations" do
+      assert [] == Aoc.Day01.k_combinations([], 0)
+      assert [] == Aoc.Day01.k_combinations([], 1)
+      assert [] == Aoc.Day01.k_combinations([], 2)
+      assert [] == Aoc.Day01.k_combinations([], -1)
+    end
+
+    test "[1,2,3] has three 1-combinations" do
+      assert [[1], [2], [3]] == Aoc.Day01.k_combinations([1, 2, 3], 1)
+    end
+
+    test "[1,2,3] has one 3-combination" do
+      assert [[1, 2, 3]] == Aoc.Day01.k_combinations([1, 2, 3], 3)
+    end
+
+    test "[1,2,3] has one 2-combination" do
+      assert [[1, 2], [1, 3], [2, 3]] == Aoc.Day01.k_combinations([1, 2, 3], 2)
+    end
   end
 
-  test "1, 2 and 3 don't sum2 to 6" do
-    assert :error == Aoc.Day01.sum2_to(6, [1, 2, 3])
+  describe "&sum2_to/2" do
+    test "1 and 3 sum2 to 4" do
+      assert {:ok, [1, 3]} == Aoc.Day01.sum2_to(4, [1, 2, 3, 4])
+    end
+
+    test "1, 2 and 3 don't sum2 to 6" do
+      assert :error == Aoc.Day01.sum2_to(6, [1, 2, 3])
+    end
+
+    test "1 and 5 sum2 to 6" do
+      assert {:ok, [1, 5]} == Aoc.Day01.sum2_to(6, [1, 2, 3, 4, 5])
+    end
+
+    test "2 does not sum2 to 4" do
+      assert :error == Aoc.Day01.sum2_to(4, [2])
+    end
   end
 
-  test "2 does not sum2 to 4" do
-    assert :error == Aoc.Day01.sum2_to(4, [2])
+  describe "&sum3_to/2" do
+    test "[1,2,3,4] don't sum3 to 4" do
+      assert :error == Aoc.Day01.sum3_to(4, [1, 2, 3, 4])
+    end
+
+    test "1, 2 and 3 sum3 to 6" do
+      assert {:ok, [1, 2, 3]} == Aoc.Day01.sum3_to(6, [1, 2, 3, 4, 5])
+    end
+
+    test "2 doesn't sum3 to 4" do
+      assert :error == Aoc.Day01.sum3_to(4, [2])
+    end
   end
 end
