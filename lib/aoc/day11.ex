5ce775673da0b1de2c8e66c0d7a1ddaed54e972c
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-12-12 17:28:46 +0100

    Finish day11 part1

---
+++ lib/aoc/day11.ex
@@ -0,0 +1,62 @@
+defmodule Aoc.Day11 do
+  def task1(input) do
+    input
+    |> Enum.map(&String.graphemes/1)
+    |> find_stable_configuration()
+    |> Enum.map(fn row -> Enum.count(row, fn e -> e == "#" end) end)
+    |> Enum.sum()
+  end
+
+  defp find_stable_configuration(seats) do
+    next_seats = next_iteration(seats)
+
+    if next_seats == seats do
+      next_seats
+    else
+      find_stable_configuration(next_seats)
+    end
+  end
+
+  def next_iteration(seats) do
+    seats
+    |> pad_seats()
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&Enum.zip/1)
+    |> Enum.map(&Enum.chunk_every(&1, 3, 1, :discard))
+    |> enum_map_2d(&extract_neighbourhood/1)
+    |> enum_map_2d(&next_element/1)
+  end
+
+  defp extract_neighbourhood([{a, b, c}, {d, e, f}, {g, h, i}]) do
+    {e, [a, b, c, d, f, g, h, i]}
+  end
+
+  defp enum_map_2d(enum, fun) do
+    Enum.map(enum, &Enum.map(&1, fun))
+  end
+
+  defp next_element({".", _}), do: "."
+
+  defp next_element({"L", neighbours}) do
+    if Enum.any?(neighbours, fn neighbour -> neighbour == "#" end) do
+      "L"
+    else
+      "#"
+    end
+  end
+
+  defp next_element({"#", neighbours}) do
+    if Enum.count(neighbours, fn neighbour -> neighbour == "#" end) >= 4 do
+      "L"
+    else
+      "#"
+    end
+  end
+
+  def pad_seats(seats) do
+    rows = Enum.map(seats, fn row -> ["."] ++ row ++ ["."] end)
+    padding_row = rows |> hd |> Enum.map(fn _ -> "." end)
+
+    [padding_row] ++ rows ++ [padding_row]
+  end
+end
---
+++ test/aoc/day11_test.exs
@@ -0,0 +1,76 @@
+defmodule Aoc.Day11Test do
+  use ExUnit.Case
+
+  alias Aoc.Day11
+
+  describe "&task1/1" do
+    test "example 1" do
+      input = [
+        "#.##.##.##",
+        "#######.##",
+        "#.#.#..#..",
+        "####.##.##",
+        "#.##.##.##",
+        "#.#####.##",
+        "..#.#.....",
+        "##########",
+        "#.######.#",
+        "#.#####.##"
+      ]
+
+      assert 37 == Day11.task1(input)
+    end
+  end
+
+  describe "&next_iteration/1" do
+    test "given only an empty seat" do
+      assert [["#"]] == Day11.next_iteration([["L"]])
+    end
+
+    test "given only floor" do
+      assert [["."]] == Day11.next_iteration([["."]])
+    end
+
+    test "given only a taken seat" do
+      assert [["#"]] == Day11.next_iteration([["#"]])
+    end
+
+    test "given taken and free seat" do
+      assert [["L", "#"]] == Day11.next_iteration([["L", "#"]])
+    end
+
+    test "given only free seats" do
+      assert [["#", "#"], ["#", "#"]] == Day11.next_iteration([["L", "L"], ["L", "L"]])
+    end
+  end
+
+  describe "&pad_seats/1" do
+    test "given one element" do
+      input = [["L"]]
+
+      expected = [
+        [".", ".", "."],
+        [".", "L", "."],
+        [".", ".", "."]
+      ]
+
+      assert expected == Day11.pad_seats(input)
+    end
+
+    test "given a grid of elements" do
+      input = [
+        ["L", ".", "#"],
+        [".", "#", "L"]
+      ]
+
+      expected = [
+        [".", ".", ".", ".", "."],
+        [".", "L", ".", "#", "."],
+        [".", ".", "#", "L", "."],
+        [".", ".", ".", ".", "."]
+      ]
+
+      assert expected == Day11.pad_seats(input)
+    end
+  end
+end
