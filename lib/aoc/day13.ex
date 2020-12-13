5e23c7fe0ee14976ef7cabdf20ca8fdc7b940cbc
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-12-13 16:26:04 +0100

    Finish day13 part2

--- lib/aoc/day13.ex
+++ lib/aoc/day13.ex
@@ -12,6 +12,12 @@ defmodule Aoc.Day13 do
     earliest_bus * (earliest_timestamp - timestamp)
   end
 
+  def task2(input) do
+    input
+    |> parse_input2()
+    |> crt_solve()
+  end
+
   def parse_input([timestamp, busses]) do
     {
       String.to_integer(timestamp),
@@ -22,7 +28,55 @@ defmodule Aoc.Day13 do
     }
   end
 
+  def parse_input2([_, busses]) do
+    busses
+    |> String.split(",")
+    |> Enum.with_index()
+    |> Enum.filter(fn
+      {"x", _} -> false
+      _ -> true
+    end)
+    |> Enum.map(fn {bus, time} ->
+      {-time, String.to_integer(bus)}
+    end)
+  end
+
   def nearest_departure_after(bus, timestamp) do
     timestamp + rem(bus - rem(timestamp, bus), bus)
+  end
+
+  def crt_solve(equations) do
+    equations
+    |> Enum.reduce(fn {a1, n1}, {a2, n2} ->
+      {crt_solve2({a1, n1}, {a2, n2}), n1 * n2}
+    end)
+    |> elem(0)
+  end
+
+  defp crt_solve2({a1, n1}, {a2, n2}) do
+    {x, y, 1} = extended_gcd(n1, n2)
+    solution = a2 * x * n1 + a1 * y * n2
+
+    mul = n1 * n2
+    rem(rem(solution, mul) + mul, mul)
+  end
+
+  def extended_gcd(a, b) do
+    do_extended_gcd(a, b, 1, 0, 0, 1)
+  end
+
+  defp do_extended_gcd(old_r, 0, old_s, _, old_t, _), do: {old_s, old_t, old_r}
+
+  defp do_extended_gcd(old_r, r, old_s, s, old_t, t) do
+    quotient = div(old_r, r)
+
+    do_extended_gcd(
+      r,
+      old_r - quotient * r,
+      s,
+      old_s - quotient * s,
+      t,
+      old_t - quotient * t
+    )
   end
 end
--- test/aoc/day13_test.exs
+++ test/aoc/day13_test.exs
@@ -5,9 +5,33 @@ defmodule Aoc.Day13Test do
 
   describe "&task1/1" do
     test "example 1" do
-      input = ["939", "7,13,x,x,59,x,31,19"]
+      assert 295 == Day13.task1(["939", "7,13,x,x,59,x,31,19"])
+    end
+  end
 
-      assert 295 == Day13.task1(input)
+  describe "&task2/1" do
+    test "example 1" do
+      assert 1_068_781 == Day13.task2(["939", "7,13,x,x,59,x,31,19"])
+    end
+
+    test "example 2" do
+      assert 3417 == Day13.task2(["", "17,x,13,19"])
+    end
+
+    test "example 3" do
+      assert 754_018 == Day13.task2(["", "67,7,59,61"])
+    end
+
+    test "example 4" do
+      assert 779_210 == Day13.task2(["", "67,x,7,59,61"])
+    end
+
+    test "example 5" do
+      assert 1_261_476 == Day13.task2(["", "67,7,x,59,61"])
+    end
+
+    test "example 6" do
+      assert 1_202_161_486 == Day13.task2(["", "1789,37,47,1889"])
     end
   end
 
@@ -16,6 +40,14 @@ defmodule Aoc.Day13Test do
       input = ["939", "7,13,x,x,59,x,31,19"]
 
       assert {939, [7, 13, 59, 31, 19]} == Day13.parse_input(input)
+    end
+  end
+
+  describe "&parse_input2/1" do
+    test "example 1" do
+      input = ["939", "7,13,x,x,59,x,31,19"]
+
+      assert [{0, 7}, {-1, 13}, {-4, 59}, {-6, 31}, {-7, 19}] == Day13.parse_input2(input)
     end
   end
 
@@ -29,6 +61,36 @@ defmodule Aoc.Day13Test do
       assert 16 == Day13.nearest_departure_after(4, 15)
       assert 22 == Day13.nearest_departure_after(11, 21)
       assert 944 == Day13.nearest_departure_after(59, 939)
+    end
+  end
+
+  describe "&crt_solve/1" do
+    test "given 2 equations" do
+      assert 3 == Day13.crt_solve([{0, 3}, {3, 4}])
+      assert 3 == Day13.crt_solve([{0, 3}, {-1, 4}])
+    end
+
+    test "given 3 equations" do
+      assert 39 == Day13.crt_solve([{0, 3}, {3, 4}, {4, 5}])
+    end
+
+    test "example" do
+      assert 3417 == Day13.crt_solve([{0, 17}, {11, 13}, {16, 19}])
+      assert 3417 == Day13.crt_solve([{0, 17}, {-2, 13}, {-3, 19}])
+    end
+  end
+
+  describe "&extended_gcd/2" do
+    test "example 1" do
+      assert {-1, 1, 1} == Day13.extended_gcd(3, 4)
+    end
+
+    test "example 2" do
+      assert {1, -1, 2} == Day13.extended_gcd(6, 4)
+    end
+
+    test "example 3" do
+      assert {-9, 2, 1} == Day13.extended_gcd(13, 59)
     end
   end
 end
