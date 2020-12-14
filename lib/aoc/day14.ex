b0726406409fcb54faf17ba845c6ee0547d6e899
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-12-14 13:44:30 +0100

    Finish day14 part2

--- lib/aoc/day14.ex
+++ lib/aoc/day14.ex
@@ -2,9 +2,17 @@ defmodule Aoc.Day14 do
   use Bitwise
 
   def task1(input) do
+    task_with_reducer(input, &mem_reducer/2)
+  end
+
+  def task2(input) do
+    task_with_reducer(input, &mem_reducer2/2)
+  end
+
+  defp task_with_reducer(input, reducer) do
     input
     |> Enum.map(&parse_line/1)
-    |> Enum.reduce({"", %{}}, &mem_reducer/2)
+    |> Enum.reduce({"", %{}}, reducer)
     |> elem(1)
     |> Map.values()
     |> Enum.sum()
@@ -14,6 +22,22 @@ defmodule Aoc.Day14 do
 
   defp mem_reducer({:mem, address, value}, {mask, memory}) do
     {mask, Map.put(memory, address, apply_mask(value, mask))}
+  end
+
+  defp mem_reducer2({:mask, mask}, {_, memory}), do: {mask, memory}
+
+  defp mem_reducer2({:mem, address, value}, {mask, memory}) do
+    addresses =
+      address
+      |> Integer.to_string(2)
+      |> String.pad_leading(36, "0")
+      |> floating_address(mask)
+      |> real_addresses()
+
+    {
+      mask,
+      Enum.reduce(addresses, memory, &Map.put(&2, &1, value))
+    }
   end
 
   def parse_line(line) do
@@ -36,5 +60,34 @@ defmodule Aoc.Day14 do
     and_filter = mask |> String.replace("X", "1") |> String.to_integer(2)
 
     (number ||| or_filter) &&& and_filter
+  end
+
+  def floating_address(address, mask) do
+    Enum.zip(String.graphemes(address), String.graphemes(mask))
+    |> Enum.map(fn
+      {v, "0"} -> v
+      {_, m} -> m
+    end)
+    |> Enum.join()
+  end
+
+  def real_addresses(floating_address) do
+    do_real_addresses([floating_address])
+  end
+
+  defp do_real_addresses(addresses) do
+    if Regex.match?(~r/X/, hd(addresses)) do
+      do_real_addresses(
+        addresses
+        |> Enum.flat_map(fn address ->
+          [
+            String.replace(address, "X", "0", global: false),
+            String.replace(address, "X", "1", global: false)
+          ]
+        end)
+      )
+    else
+      addresses
+    end
   end
 end
--- test/aoc/day14_test.exs
+++ test/aoc/day14_test.exs
@@ -14,6 +14,17 @@ defmodule Aoc.Day14Test do
     assert 165 == Day14.task1(input)
   end
 
+  describe "&task2/1" do
+    input = [
+      "mask = 000000000000000000000000000000X1001X",
+      "mem[42] = 100",
+      "mask = 00000000000000000000000000000000X0XX",
+      "mem[26] = 1"
+    ]
+
+    assert 208 == Day14.task2(input)
+  end
+
   describe "&parse_line/1" do
     test "memory instruction" do
       assert {:mem, 8, 23} == Day14.parse_line("mem[8] = 23")
@@ -33,6 +44,32 @@ defmodule Aoc.Day14Test do
     test "example 2" do
       assert String.to_integer("10111", 2) ==
                Day14.apply_mask(String.to_integer("10101"), "10X1X")
+    end
+  end
+
+  describe "&floating_address/2" do
+    test "example 1" do
+      assert "0X1101X" == Day14.floating_address("0101010", "0X1001X")
+    end
+
+    test "example 2" do
+      assert "01X0XX" == Day14.floating_address("011010", "00X0XX")
+    end
+  end
+
+  describe "&real_addresses/1" do
+    test "given a non floating address" do
+      assert MapSet.equal?(
+               MapSet.new(["0101"]),
+               MapSet.new(Day14.real_addresses("0101"))
+             )
+    end
+
+    test "given a floating address" do
+      assert MapSet.equal?(
+               MapSet.new(["0001", "0011", "0101", "0111"]),
+               MapSet.new(Day14.real_addresses("0XX1"))
+             )
     end
   end
 end
