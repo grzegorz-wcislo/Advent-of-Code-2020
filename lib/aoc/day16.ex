b32437609bcd86290e3134f33c183748f2d2e4fa
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2021-06-26 19:33:28 +0200

    Finish day16 part2

--- lib/aoc/day16.ex
+++ lib/aoc/day16.ex
@@ -4,20 +4,53 @@ defmodule Aoc.Day16 do
   def task1(input) do
     {rules, _, tickets} = parse_input(input)
 
-    rule_tests =
-      rules
-      |> Enum.map(fn {_, range1, range2} ->
-        fn x -> Enum.member?(range1, x) or Enum.member?(range2, x) end
-      end)
-
     tickets
     |> Enum.flat_map(&Function.identity/1)
-    |> Enum.filter(&Enum.all?(rule_tests, fn test -> not test.(&1) end))
+    |> Enum.filter(fn value -> Enum.all?(rules, &(!value_valid?(value, &1))) end)
     |> Enum.sum()
+  end
+
+  def task2(input) do
+    {rules, my_ticket, tickets} = parse_input(input)
+
+    valid_tickets = Enum.filter(tickets, &ticket_valid?(&1, rules))
+
+    field_order(valid_tickets, rules)
+    |> Enum.zip(my_ticket)
+    |> Enum.filter(fn {name, _} -> String.starts_with?(name, "departure") end)
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.product()
   end
 
   def parse_input(input) do
     do_parse_rules(input, [])
+  end
+
+  def field_order(tickets, rules) do
+    columns = tickets |> Enum.zip() |> Enum.map(&Tuple.to_list/1)
+
+    columns
+    |> Enum.with_index()
+    |> Enum.map(fn {column, index} ->
+      fields = possible_fields(column, rules)
+      {length(fields), index, fields}
+    end)
+    |> Enum.sort()
+    |> Enum.reduce([[], []], fn {_, index, fields}, [indices, resolved_fields] ->
+      [field] =
+        MapSet.difference(MapSet.new(fields), MapSet.new(resolved_fields)) |> MapSet.to_list()
+
+      [[index | indices], [field | resolved_fields]]
+    end)
+    |> Enum.zip()
+    |> Enum.sort()
+    |> Enum.map(&elem(&1, 1))
+  end
+
+  def possible_fields(values, rules) do
+    rules
+    |> Enum.filter(fn rule -> Enum.all?(values, &value_valid?(&1, rule)) end)
+    |> Enum.map(&elem(&1, 0))
   end
 
   defp do_parse_rules(["", _ | tail], acc) do
@@ -46,5 +79,16 @@ defmodule Aoc.Day16 do
 
   defp parse_ticket(ticket) do
     ticket |> String.split(",") |> Enum.map(&String.to_integer/1)
+  end
+
+  defp value_valid?(value, {_, range1, range2}) do
+    Enum.member?(range1, value) or Enum.member?(range2, value)
+  end
+
+  defp ticket_valid?(ticket, rules) do
+    ticket
+    |> Enum.all?(fn value ->
+      Enum.any?(rules, &value_valid?(value, &1))
+    end)
   end
 end
--- test/aoc/day16_test.exs
+++ test/aoc/day16_test.exs
@@ -47,4 +47,36 @@ defmodule Aoc.Day16Test do
       assert [[7, 3, 47], [40, 4, 50], [55, 2, 20], [38, 6, 12]] == tickets
     end
   end
+
+  describe "&field_order/2" do
+    test "example 1" do
+      tickets = [
+        [3, 9, 18],
+        [15, 1, 5],
+        [5, 14, 9]
+      ]
+
+      rules = [
+        {"class", 0..1, 4..19},
+        {"row", 0..5, 8..19},
+        {"seat", 0..13, 16..19}
+      ]
+
+      assert ["row", "class", "seat"] == Day16.field_order(tickets, rules)
+    end
+  end
+
+  describe "&possible_rules/2" do
+    test "example 1" do
+      rules = [
+        {"class", 0..1, 4..19},
+        {"row", 0..5, 8..19},
+        {"seat", 0..13, 16..19}
+      ]
+
+      assert ["row"] == Day16.possible_fields([3, 15, 5, 11], rules)
+      assert ["class", "row"] == Day16.possible_fields([9, 1, 14, 12], rules)
+      assert ["class", "row", "seat"] == Day16.possible_fields([8, 5, 9, 13], rules)
+    end
+  end
 end
